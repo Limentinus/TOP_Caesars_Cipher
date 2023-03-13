bbf621c307e6c358e8d0252eff97555e3532a18a
LughsClues <cuberfreak@gmail.com>
2023-03-13 15:18:02 +0100

    Test multiple words

--- spec/ceasarcipher_spec.rb
+++ spec/ceasarcipher_spec.rb
@@ -4,4 +4,8 @@ describe "#caesar_cipher" do
   it "returns lowercase characters shifted by 1" do
     expect(caesar_cipher("test", 1)).to eql("uftu")
   end
+
+  it "returns ciphered words with spaces in their places" do
+    expect(caesar_cipher("test another sentence", 1)).to eql("uftu bopuifs tfoufodf")
+  end
 end
