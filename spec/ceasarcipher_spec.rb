402ffdaba0370fbcb12e8bda2ec7a8f44e81b840
LughsClues <cuberfreak@gmail.com>
2023-03-13 15:20:23 +0100

    Test that punctuation is unaltered

--- spec/ceasarcipher_spec.rb
+++ spec/ceasarcipher_spec.rb
@@ -8,4 +8,8 @@ describe "#caesar_cipher" do
   it "returns ciphered words with spaces in their places" do
     expect(caesar_cipher("test another sentence", 1)).to eql("uftu bopuifs tfoufodf")
   end
+
+  it "returns punctuation unaltered" do
+    expect(caesar_cipher(".,?", 1)).to eql(".,?")
+  end
 end
